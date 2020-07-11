2c03034511dcf966891d123ad3a503a8fd35a706
lyrahgames <markuspawellek@gmail.com>
2020-07-11 12:49:56 +0200

    Update site

---
+++ javascripts/config.js
@@ -0,0 +1,13 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [['\\(', '\\)']],
+    displayMath: [['\\[', '\\]']],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {ignoreHtmlClass: '.*|', processHtmlClass: 'arithmatex'}
+
+};
+
+MathJax.Hub.Config(
+    {jax: ['input/TeX', 'output/HTML-CSS'], displayAlign: 'left'});
